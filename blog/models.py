41497cea6887da34a203a6c98b99c2dae281df92
Sine <ycherifi204@gmail.com>
2023-11-24 11:51:03 +0100

    Adding a comment section and approving by admin.

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,9 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
     def __str__(self):
         # if len(self.text) >= 50:
@@ -20,4 +23,22 @@ class Post(models.Model):
         # else:
         #     return self.text 
         return self.title
-        
+
+class Comment(models.Model):
+    """Adding comment section to the blog"""
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        """Approving the comment"""
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        """Shows the text"""
+        return self.text
+
+
